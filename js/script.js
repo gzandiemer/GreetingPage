9ed0127ea6799409da22f917828cc3d6300d6fc3
Günışığı Zan Diemer <43114601+gzandiemer@users.noreply.github.com>
2019-03-22 15:01:53 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-/*js is case sensitive!!*/
 var today = new Date(); //create a new date object
 var hourNow = today.getHours(); //find the current hour
 var dd = today.getDate();
@@ -11,24 +10,18 @@ var salutation;
 var body = document.getElementsByTagName('body')[0]; //for the cover photo in body
 //display the appropriate greeting based on the current time
 if (hourNow > 18) {
-/*blue part determines which code will run and purple curly braces indicate the start and end of the code block*/
- gruesse = 'Guten Abend' ;//statement
- /*each statement should start on a new line and end with a semicolon*/
- greeting = 'Good Evening'; //= is to assign a value to the variable
- selamlama = 'İyi akşamlar'; //this a string data type
-/* string data type can be in single or double quotes but it should be straight not curly, it should be on the same line; and opening and closing one should match.
-If there will be an additional quote within the string data type then surrounding quote should be the other type*/
-/*in js no need to specify the data type*/
- salutation = 'Bonsoir';
- body.style.backgroundImage = 'url(img/evening.jpg)';
+ 	gruesse = 'Guten Abend';
+ 	greeting = 'Good Evening'; 
+ 	selamlama = 'İyi akşamlar';
+ 	salutation = 'Bonsoir';
+ 	body.style.backgroundImage = 'url(img/evening.jpg)';
  }
 else if (hourNow > 11) {
- gruesse = 'Guten Tag';
- greeting = 'Good Afternoon';
- selamlama = 'İyi Günler';
- salutation = 'Bonne Après-midi';
- body.style.backgroundImage = 'url(img/noon.jpg)';
-/*pay attention if the file is jpg or jpeg*/
+ 	gruesse = 'Guten Tag';
+ 	greeting = 'Good Afternoon';
+ 	selamlama = 'İyi Günler';
+	 salutation = 'Bonne Après-midi';
+ 	body.style.backgroundImage = 'url(img/noon.jpg)';
  }
 
 else if (hourNow > 3) {
@@ -38,13 +31,15 @@ else if (hourNow > 3) {
  salutation = 'Bonjour';
  body.style.backgroundImage = 'url(img/sunrise.jpg)';
  }
+
 else {
-gruesse = 'Hallo!';
-greeting = 'Hello!';
-selamlama = 'Merhaba!';
-salutation = 'Salut!';
-body.style.backgroundImage = 'url(img/horizon.jpg)';
+	gruesse = 'Hallo!';
+	greeting = 'Hello!';
+	selamlama = 'Merhaba!';
+	salutation = 'Salut!';
+	body.style.backgroundImage = 'url(img/horizon.jpg)';
 }
+
 document.getElementById('js-content-gruesse').innerHTML = "<h1>" + gruesse + "</h1>";
 document.getElementById('js-content-greeting').innerHTML = "<h1>" + greeting + "</h1>";
 document.getElementById('js-content-selamlama').innerHTML = "<h1>" + selamlama + "</h1>";
@@ -57,7 +52,7 @@ if(dd<10) {
 if(mm<10) {
 	mm = '0'+mm;
 }
-today = mm + '/' + dd + '/' + yyyy;
+today = dd + '/' + mm + '/' + yyyy;
 document.getElementById('js-content-today').innerHTML = "<p>" + today + "</p>";
 
 //DRAW CLOCK
